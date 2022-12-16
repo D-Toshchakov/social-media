6271c6fafc53d56089d15886471b4a433f702ec8
D-Toshchakov <daniiltoshchakov@gmail.com>
2022-12-16 15:40:48 +0300

    added post user route handler

--- handlers/healthHandler.go
+++ handlers/healthHandler.go
@@ -2,7 +2,7 @@ package handlers
 
 import "net/http"
 
-func TestHandler(w http.ResponseWriter, r *http.Request) {
+func Health(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "Application/json")
 	w.WriteHeader(200)
 	w.Write([]byte("I'm healthy"))
--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,13 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/D-Toshchakov/pet/social-media/handlers"
 	"github.com/gorilla/mux"
 )
 
 func Setup(r *mux.Router) {
-	r.HandleFunc("/health", handlers.TestHandler)
+	r.HandleFunc("/health", handlers.Health).Methods(http.MethodGet)
+	r.HandleFunc("/users", handlers.PostUser).Methods(http.MethodPost)
 }
